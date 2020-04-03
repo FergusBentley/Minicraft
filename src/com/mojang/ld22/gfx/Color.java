f44f2de2a471388c4a48ecd909d819675c822327
FergusBentley <fergus.bentley@gmail.com>
2020-04-03 23:36:57 +0100

    Add Colour util methods

--- src/com/mojang/ld22/gfx/Color.java
+++ src/com/mojang/ld22/gfx/Color.java
@@ -14,4 +14,27 @@ public class Color {
 		return r * 36 + g * 6 + b;
 	}
 
+	public static int toHex(int c) {
+		int r = (c / 100 % 10);
+		int g = (c / 10 % 10);
+		int b = (c % 10);
+		return (r  * 51 << 16) + (g * 51 << 8) + (b * 51);
+	}
+
+	public static int red(int c) {
+		return (c / 100 % 10);
+	}
+
+	public static int green(int c) {
+		return (c / 10 % 10);
+	}
+
+	public static int blue(int c) {
+		return (c % 10);
+	}
+
+	public static int fromRGB(int r, int g, int b) {
+		return r * 100 + g * 10 + b;
+	}
+
 }
